054d1b9d0f37394f2b5e720a007fe1bc5715ac5a
aybikeustun <aybikeustun@gmail.com>
2023-04-23 22:22:43 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+const form = document.getElementsByTagName('form')[0];
+const newTaskInput = document.getElementsByTagName('input')[0];
+const taskList = document.getElementById('tasks');
+
+
+form.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+
+  const newTask = newTaskInput.value;
+
+  const listItem = document.createElement('li');
+
+  listItem.innerText = newTask;
+
+  taskList.appendChild(listItem);
+
+});
+
 });
